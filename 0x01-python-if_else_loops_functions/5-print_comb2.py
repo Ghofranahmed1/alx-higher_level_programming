e93bdb1dcb86f3440eaf445a12d80374888f6fa3
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-11-28 12:23:53 -0800

    tak 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(00,100):
+for i in range(00, 100):
     print("{}".format(str(i).zfill(2)), end="")
-    print(", ", end="")
+    if (i != 99):
+        print(", ", end="")
