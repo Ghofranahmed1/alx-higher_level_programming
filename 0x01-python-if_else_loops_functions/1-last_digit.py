96fd9c9dfee85b9ce17e0659240d86498193ee37
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-11-28 10:59:38 -0800

    task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    num_copy = number % -10
+else:
+    num_copy = number % 10
+if (num_copy > 5):
+    print("Last digit of {} is {} and is greater than 5".format(number,num_copy))
+elif(num_copy == 0):
+    print("Last digit of {} is {} and is 0".format(number,num_copy))
+else:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number,num_copy))
