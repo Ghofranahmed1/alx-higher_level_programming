6aae2c7cd31a32306b9a316493f8418929ed1e8d
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-11-28 17:01:17 -0800

    task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -18,6 +18,11 @@ listint_t *insert_node(listint_t **head, int number)
 	new->n = number;
 	new->next = NULL;
 
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 	if (rev->n > number)
 	{
 		*head = new;
