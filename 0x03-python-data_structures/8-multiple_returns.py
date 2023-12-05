593e54b3f8cf711a07124dbfb94f873c5235d49c
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-12-05 06:02:38 -0800

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    str_len = len(sentence)
-    if len == 0:
-        new_tuple = (0, None)
-    else:
-        new_tuple = (str_len, sentence[:1])
+        str_len = len(sentence)
+        if len == 0:
+            new_tuple = (0, 'None')
+        else:
+            new_tuple = (str_len, sentence[:1])
 
-    return(new_tuple)
+    return (new_tuple)
