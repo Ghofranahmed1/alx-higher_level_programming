608515ade0b00e73c3c0f86570b7fa19b0a67bfb
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-12-09 12:21:21 -0800

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not roman_string or isinstance(roman_string, str) == False:
         return 0
-    elif roman_string:
+    else:
         Sum = 0
         prevalue = 0
         my_dirc = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
