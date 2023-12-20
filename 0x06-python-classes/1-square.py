2febfb5176728427a6b1e3ac295e1daa72918f49
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-12-20 14:12:30 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" creat a square class"""
+
+
+class square:
+    """
+    Class that defines properties of square by: (based on 0-square.py).
+
+    attribute:
+        size: size of a square(1 side).
+        """
+
+    def __init__(self, size):
+        """
+        creat new instance of square
+        """
+        self.size = size
+
