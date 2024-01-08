5565eb9de53158f77bb1bfd525660db8cfafa12c
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-01-08 01:39:56 -0800

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+Rectangle = __import__('9-rectangle').Rectangle
+""" Square claa"""
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Method for initialized the attrubutes"""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """rectangle area"""
+        return self.__size ** 2
