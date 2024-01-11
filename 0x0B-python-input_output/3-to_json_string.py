ab72e4a7192e44e70160c0500a891c774cdc7563
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-01-11 07:07:09 +0000

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,9 +6,9 @@ import json
 def to_json_string(my_obj):
     """
     function that returns the JSON representation of an object
-    Args: 
+    Args:
         my_obj: the object need to convert
     Returns:
-	converted data
-	"""
+        converted data
+    """
     return json.dumps(my_obj)
