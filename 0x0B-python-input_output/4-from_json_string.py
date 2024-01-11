956b55272b184d3eaa3ac20332cb543a75215021
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-01-11 07:11:54 +0000

    task 3

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function returns the reverse JSON representation"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    function that returns the reverse JSON representation of an object
+    Args:
+        my_str: the object need to convert
+    Returns:
+        converted data
+    """
+    return json.load(my_obj)
+~                             
