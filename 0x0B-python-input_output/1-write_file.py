49afd40b9ecf4a28826996bcd787be4ac54320a7
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-01-11 06:35:16 +0000

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """i/o operation"""
+
+
 def write_file(filename="", text=""):
-	"""function that writes  to a text file (UTF8)
-	and returns the number of characters written"""
-	with open(filename, mode='w', encoding='UTF-8') as f:
-		return f.write(text)
+    """function that writes  to a text file (UTF8)
+    and returns the number of characters written"""
+    with open(filename, mode='w', encoding='UTF-8') as f:
+	return f.write(text)
