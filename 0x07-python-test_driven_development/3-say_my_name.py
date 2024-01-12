958d289bd871a1c30f921c52e528cacd94aa4882
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-01-11 23:39:27 -0800

    task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """function print names"""
+
+
 def say_my_name(first_name, last_name=""):
     """
     Say_my_name: print first and last name
@@ -9,8 +11,8 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: if the first and last name were not a strings
     """
-    if  not isinstance(first_name, str):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if  not isinstance(last_name, str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
