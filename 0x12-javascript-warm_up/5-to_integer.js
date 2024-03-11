50780df536acd57fa7b4089d341fed27f9351b34
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-03-11 19:56:20 +0000

    task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+*prints My number: <first argument converted in integer>
+*/
+
+myVar = process.argv[2];
+if (isNaN(myVar)) {
+  console.log('Not a number');
+} else {
+  console.log ('My number: ' + parseInt(myVar));
+}
