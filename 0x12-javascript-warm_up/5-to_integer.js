a44c90d313c12e28acc68ce7989d3d35b7af816a
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-03-11 20:01:02 +0000

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -7,5 +7,5 @@ const myVar = process.argv[2];
 if (isNaN(myVar)) {
   console.log('Not a number');
 } else {
-  console.log ('My number: ' + parseInt(myVar));
+  console.log('My number: ' + parseInt(myVar));
 }
