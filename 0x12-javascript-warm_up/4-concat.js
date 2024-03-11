6e77fa1a7b68ff992b961c789275f44e1a7d7b93
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-03-11 19:40:59 +0000

    task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+* two arguments passed to it, in the following format: ... is ...
+*/
+
+const varOne = process.argv[2];
+const varTwo = process.argv[3];
+console.log(varOne + ' is ' + varTwo);
