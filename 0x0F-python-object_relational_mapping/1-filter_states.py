c5ff1232058169b30343994b920245ec58b0f87d
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-03-17 11:33:02 -0700

    task 11

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,9 +6,9 @@ from sys import argv
 # The code should not be executed when imported
 if __name__ == '__main__':
 
-    #Connect to my database
+    # Connect to my database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE\
             BINARY 'N%' ORDER BY id ASC")
