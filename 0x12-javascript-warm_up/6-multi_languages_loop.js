fd64cfa214e4bf76d68294981f3af63cd9d42319
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-03-23 12:41:24 -0700

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * script that prints 3 lines
+ *  but by using an array of string and a loop
+ */
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0 ; i < 3; i++ ){
+	console.log(array[i]);
+}
+
