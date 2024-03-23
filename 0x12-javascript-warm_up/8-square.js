717bf679def6105a7f88886d64958ad28d31754b
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-03-23 13:36:47 -0700

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,6 +6,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-	  consloe.log('X'.repeat(parseInt(process.argv[2])));
+	  console.log('X'.repeat(parseInt(process.argv[2])));
   }
 }
