05b1caaaf3e35b43a5e3306d60659b95dff0e76c
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-03-28 12:03:00 -0700

    documentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+'''  Python script that fetches https://alx-intranet.hbtn.io/status '''
 import urllib.request
 
 if __name__ == "__main__":
-    '''  Python script that fetches https://alx-intranet.hbtn.io/status '''
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as f:
         html = f.read()
